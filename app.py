3e425468b6d2d2aa58f89d7b1acf43ea8091bae7
xaldovah <dxaldovah@gmail.com>
2024-07-02 16:13:55 +0300

    added a function to retrive the actual api of the user

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 from flask import Flask, request, jsonify
 import requests
 
-
 app = Flask(__name__)
 
-
 WEATHER_API_KEY = '814ff9aed0f8900a97ae0832af2cd3a4'
+GEOLOCATION_API_KEY = 'f188689a3c6b4a8c976fa00150cf7ce4'
+
+def get_client_ip():
+    if 'X-Forwarded-For' in request.headers:
+        return request.headers['X-Forwarded-For'].split(',')[0]
+    return request.remote_addr
 
 
 @app.route('/api/hello')
 def hello():
     visitor_name = request.args.get('visitor_name', 'visitor')
-    client_ip = request.remote_addr
+    client_ip = get_client_ip()
 
-    geo_response = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey=f188689a3c6b4a8c976fa00150cf7ce4')
+    # Use the ipgeolocation.io API to get location data
+    geo_response = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey={GEOLOCATION_API_KEY}')
     geo_data = geo_response.json()
-    city = geo_data.get('city')
+    city = geo_data.get('city', 'Unknown')
 
+    # Use the OpenWeatherMap API to get weather data
     weather_response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric')
     weather_data = weather_response.json()
     temperature = weather_data['main']['temp']
-
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
 
     return jsonify({
